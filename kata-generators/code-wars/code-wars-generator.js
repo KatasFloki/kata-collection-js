c753415650b47e74d34ad9ae0c6dbb0bc2ce4e11
Alejandro Dietta <alexdietta@hotmail.com>
2019-12-16 21:30:35 +0100

    First kata added

--- kata-generators/code-wars/code-wars-generator.js
+++ kata-generators/code-wars/code-wars-generator.js
@@ -1,10 +1,19 @@
 const cheerio = require('cheerio');
 const axios = require("axios");
 
-axios.get('https://www.codewars.com/kata/beginner-lost-without-a-map/train/javascript')
+axios.get('https://www.codewars.com/kata/projects/5df6a848c6ba5100142529a6/javascript/session')
   .then((response) => {
   // Load the web page source code into a cheerio instance
-  const $ = cheerio.load(response.data)
+  console.log(response.data)
   
-  console.log($.html())
-})
+  
+  /*const kyu = $('.inner-small-hex.is-extra-wide span').text();
+  const title = $('h4.mbs.is-white-text').eq(0).text();
+  const description = $('#description').text();
+  
+  console.log(kyu)
+  console.log(title)
+  console.log(description)*/
+
+  
+});
