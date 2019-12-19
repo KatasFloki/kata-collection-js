e54fc036e5818e1121768b361b40da70bc6feada
Alejandro Dietta <alexdietta@hotmail.com>
2019-12-19 23:29:52 +0100

    First approach to code-wars kata generator with puppeteer and jquery

--- kata-generators/code-wars/code-wars-generator.js
+++ kata-generators/code-wars/code-wars-generator.js
@@ -1,19 +1,54 @@
-const cheerio = require('cheerio');
-const axios = require("axios");
+const fs = require('fs');
+const puppeteer = require('puppeteer');
 
-axios.get('https://www.codewars.com/kata/projects/5df6a848c6ba5100142529a6/javascript/session')
-  .then((response) => {
-  // Load the web page source code into a cheerio instance
-  console.log(response.data)
-  
-  
-  /*const kyu = $('.inner-small-hex.is-extra-wide span').text();
-  const title = $('h4.mbs.is-white-text').eq(0).text();
-  const description = $('#description').text();
-  
-  console.log(kyu)
-  console.log(title)
-  console.log(description)*/
+const BASE_URL = 'https://www.codewars.com/kata/'
 
+const kataInformationParser = ({ id, title, description, code }) => {
+  return `
+/*
+Kata Id: ${id}
+
+Title:
+${title}
+
+Intructions:
+${description}
+
+Link: ${BASE_URL}${id}
+
+Solution
+${code}
+*/
+`
+}
+
+
+
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.codewars.com/kata/generate-range-of-integers/train/javascript', {waitUntil: 'networkidle0'});
+  await page.addScriptTag({ url: 'https://code.jquery.com/jquery-3.2.1.min.js' });
+
+  let username = await page.evaluate(() => {
+    const $ = window.$;
+    const kyu = $('.inner-small-hex.is-extra-wide span').eq(2).text().split(" ")[0]
+    const title = $('h4.mbs.is-white-text').eq(0).text();
+    const description = $('#description').text();
+    
+    return { kyu, title, description }
+  }) 
   
-});
+  console.log(username)
+  
+  browser.close();
+
+  fs.writeFile('test.js', kataInformationParser(username), function (err) {
+    if (err) throw err;
+    console.log('File is created successfully.');
+  }); 
+}
+
+run();
